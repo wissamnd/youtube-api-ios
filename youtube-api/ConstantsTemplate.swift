afccd74abb5a62d42de6b17deadff6f641949d8b
wissamnoureddine <wissam62500@gmail.com>
2020-07-05 20:59:03 +0300

    modified gitignore to stop tracking Constants.swift added a template instead

--- youtube-api/ConstantsTemplate.swift
+++ youtube-api/ConstantsTemplate.swift
@@ -12,10 +12,10 @@
 
 import Foundation
 
-//struct Constants {
-//
-//    static var API_KEY = ""
-//    static var PLAYLIST_ID = ""
-//    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
-//
-//}
+struct Constants {
+
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+
+}
